cc7a4224da974f8f462d759ab4eac33b47fd0a8b
Timo Gronau <timogronau@gmail.com>
2023-10-09 11:39:55 +0200

    initialized, structurized, basic templates, projects app

---
+++ devsearch/projects/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+import uuid
+
+from inherit_me.models import CreatedModifiedDateTime
+
+
+
+
+class Project(CreatedModifiedDateTime):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) #blank for django, null for database
+    demo_link = models.CharField(max_length=2000,null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True) #need quotes bc Tag's below
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(CreatedModifiedDateTime):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    #owner =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(CreatedModifiedDateTime):
+    name = models.CharField(max_length=200)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
